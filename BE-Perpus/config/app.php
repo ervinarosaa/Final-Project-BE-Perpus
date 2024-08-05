cd3238b3a3b89b82877c2ae2a20f4ba116ce299c
ervinarosaa <ervinarosaaulia@gmail.com>
2024-08-05 16:41:00 +0700

    InstalConfig Cloudinary & Change APP_URL

--- BE-Perpus/config/app.php
+++ BE-Perpus/config/app.php
@@ -194,7 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider::class,
     ],
 
     /*
@@ -211,5 +211,8 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
-
+    
+    'aliases' => [
+        'Cloudinary' => CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary::class,
+    ],
 ];
